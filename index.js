4507f62a2d4639410c317b3cb983c1f54cdfb252
Mathieu Schroeter <schroeter@epsitec.ch>
2015-03-25 15:52:44 +0100

    Revert "Define the server namespace in the bus config file."
    This reverts commit c3215d58eeb48e3583ca7029cd2e7d03ee8d2499.
    
    Conflicts:
    	lib/xcraft-core-bus/lib/commander.js
    	lib/xcraft-core-busclient/index.js

--- index.js
+++ index.js
@@ -173,9 +173,4 @@ exports.xcraftConfig = [{
     return /^[0-9]{1,}$/.test (value);
   },
   default: '9200'
-}, {
-  type: 'input',
-  name: 'namespaceServer',
-  message: 'namespace for server broadcast events',
-  default: 'greathall'
 }];
