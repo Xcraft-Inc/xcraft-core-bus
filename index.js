d871fe2af53e3270dedb78a5e5dbc707fabff236
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-01 17:23:45 +0200

    Move all configurations in dedicated files.

--- index.js
+++ index.js
@@ -144,29 +144,3 @@ exports.stop = function () {
   busCommander.stop ();
   busNotifier.stop ();
 };
-
-/**
- * Retrieve the inquirer definition for xcraft-core-etc
- */
-exports.xcraftConfig = [{
-  type: 'input',
-  name: 'host',
-  message: 'hostname or IP',
-  default: '127.0.0.1'
-}, {
-  type: 'input',
-  name: 'commanderPort',
-  message: 'commander port',
-  validate: function (value) {
-    return /^[0-9]{1,}$/.test (value);
-  },
-  default: '9100'
-}, {
-  type: 'input',
-  name: 'notifierPort',
-  message: 'notifier port',
-  validate: function (value) {
-    return /^[0-9]{1,}$/.test (value);
-  },
-  default: '9300'
-}];
