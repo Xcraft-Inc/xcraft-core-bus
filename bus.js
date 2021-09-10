883e29e2cd6d48a92c805b124037a52385c8def9
Mathieu Schroeter <mathieu@schroetersa.ch>
2021-09-10 14:47:14 +0200

    Remove broken and deprecated watcher xBus commands

--- bus.js
+++ bus.js
@@ -4,7 +4,6 @@ const path = require('path');
 const xBus = require('.');
 
 const cmds = {};
-const watched = {};
 
 let appId = '$';
 let tribe = '';
@@ -76,74 +75,6 @@ cmds['module.reload'] = function* (msg, resp) {
   }
 
   resp.events.send(`bus.module.reload.${msg.id}.finished`);
-};
-
-cmds['module.watch'] = function* (msg, resp, next) {
-  const chokidar = require('chokidar');
-  const {file} = msg.data;
-  const files = getModuleFiles(file);
-
-  const dirList = new Set();
-
-  for (const file of files) {
-    const dirname = path.dirname(file);
-
-    if (dirList.has(dirname)) {
-      continue;
-    }
-
-    dirList.add(dirname);
-
-    if (watched[dirname]) {
-      watched[dirname].handle.close();
-    }
-
-    const _next = next.parallel();
-
-    watched[dirname] = {
-      handle: chokidar
-        .watch(dirname)
-        .on('all', (event, location) => {
-          if (!watched[dirname].ready) {
-            return;
-          }
-
-          resp.log.info(`file ${location} has changed, reload...`);
-          const modFiles = files.filter((file) => file.startsWith(dirname));
-          resp.command.send('bus.module.reload', {files: modFiles}, () => {});
-        })
-        .on('ready', () => {
-          watched[dirname].ready = true;
-          _next();
-        })
-        .on('error', (err) => {
-          resp.log.err(err);
-          _next(err);
-        }),
-      ready: false,
-    };
-  }
-
-  yield next.sync();
-
-  resp.log.verb(`watched modules: ${Object.keys(watched).join(', ')}`);
-  resp.events.send(`bus.module.watch.${msg.id}.finished`);
-};
-
-cmds['module.unwatch'] = function (msg, resp) {
-  const {file} = msg.data;
-  const dirnames = getModuleFiles(file).map((file) => path.dirname(file));
-
-  dirnames
-    .filter((dirname) => !!watched[dirname])
-    .forEach((dirname) => {
-      resp.log.info(`stop watching for ${dirname}`);
-      watched[dirname].handle.close();
-      delete watched[dirname];
-    });
-
-  resp.log.verb(`watched modules: ${Object.keys(watched).join(', ')}`);
-  resp.events.send(`bus.module.unwatch.${msg.id}.finished`);
 };
 
 cmds.xcraftMetrics = function (msg, resp) {
@@ -304,24 +235,6 @@ exports.xcraftCommands = function () {
         options: {
           params: {
             optional: 'files...',
-          },
-        },
-      },
-      'module.watch': {
-        parallel: true,
-        desc: 'start watching for module auto-reload',
-        options: {
-          params: {
-            optional: 'file',
-          },
-        },
-      },
-      'module.unwatch': {
-        parallel: true,
-        desc: 'stop watching for module auto-reload',
-        options: {
-          params: {
-            optional: 'file',
           },
         },
       },
